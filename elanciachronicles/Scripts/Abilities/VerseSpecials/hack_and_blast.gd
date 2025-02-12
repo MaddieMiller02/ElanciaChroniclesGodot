a75e3c7c4baa812173c32cd2d7ea47db335f3b6e
MaddieMiller02 <maddie.miller@du.edu>
2025-02-12 13:46:57 -0700

    Hack and Blast
    Added Hack and Blast special for Verse

--- elanciachronicles/Scripts/Abilities/VerseSpecials/hack_and_blast.gd
+++ elanciachronicles/Scripts/Abilities/VerseSpecials/hack_and_blast.gd
@@ -1,11 +1,27 @@
 extends Ability
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func perform_ability(User:BattleCharacter, Target:BattleCharacter, CurrentManager:BattleManager):
+	# Calculations
+	var DamageOffset = randi_range(-2, 2)
+	var Damage = ((User.Strength + Power) - (Target.Defense + User.TempDefense)) + DamageOffset
+	var HitChance = (HitRate + User.Speed) - Target.Speed
+	var HitRoll = randi_range(0, 100)
+	
+	print("Calculated Damage: " + str(Damage))
+	print("Calculated Hit Chance: " + str(HitChance))
+	print("Calculated Hit Roll: " + str(HitRoll))
+	print("User Starting AP: " + str(User.CurrentAP))
+	print("Enemy Starting HP: " + str(Target.CurrentHP))
+	
+	# Perform calculated outcome and dispay text boxes
+	var TextBox = TextBoxScene.instantiate()
+	self.add_child(TextBox)
+	if HitRoll < HitChance:
+		Target.take_damage(Damage)
+		TextBox.display_damage_message(self, User, Target, Damage)
+	else:
+		TextBox.display_missed_message(self, User)
+	User.use_ap(APCost)
+	
+	print("User Ending AP: " + str(User.CurrentAP))
+	print("Enemy Ending HP: " + str(Target.CurrentHP))
